47f9076b0c049b603e94d184f8db1b37a1da5247
Bas Kuipers <bas.kuipers@me.com>
2019-10-11 15:41:44 +0200

    Added Base ui components.
    (Perhaps autoload soon?)

---
+++ client/src/components/base/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="base-button" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'base-button',
+};
+</script>
+
+<style lang="scss" scoped>
+  .base-button {
+    background-color: #24B69E;
+    color: white;
+    padding: .7em 3em;
+    border-radius: 5em;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #1E8574;
+    }
+
+    &:active {
+      opacity: .75;
+    }
+  }
+</style>
---
+++ client/src/components/base/BaseInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="base-input">
+    <label class="label">{{ label }}</label>
+    <input class="input" v-bind="$attrs" :value="value" v-on="$listeners">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'base-input',
+  inheritAttrs: false,
+  props: {
+    value: {
+      required: false,
+    },
+    label: {
+      required: true,
+    },
+  },
+  computed: {
+    listeners() {
+      const { input, ...listeners } = this.$listeners;
+      return listeners;
+    },
+  },
+  methods: {
+    input(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .base-input {
+    position: relative;
+
+    .input {
+      background: #282848;
+      display: block;
+      width: 100%;
+      box-sizing: border-box;
+      border: none;
+      border-radius: 2px;
+      padding: 2em 1em 1em 1em;
+      color: white;
+      font-size: 1rem;
+    }
+
+    .label {
+      position: absolute;
+      top: .75rem;
+      left: 1rem;
+      font-size: .75rem;
+      color: #CCCCE8;
+    }
+  }
+</style>
